102ed34beb7f0de20f2e0647317a3ca64fb6432c
Igor Redchuk <redchuk@gmail.com>
2017-07-20 22:02:41 +0200

    Remove duplication in webpack configs

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,31 @@
-module.exports = env => require(`./webpack.${env || 'dev'}.js`)
+const path = require('path')
+
+const commonConfig = {
+  entry: './src/main.js',
+
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            plugins: 'transform-runtime',
+            presets: ['env', 'react']
+          }
+        }
+      }
+    ]
+  }
+}
+
+module.exports = env => {
+  const envConfig = require(`./webpack.${env || 'dev'}.js`)
+  return Object.assign(commonConfig, envConfig)
+}
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,9 +1,6 @@
-const path = require('path')
 const webpack = require('webpack')
 
 module.exports = {
-  entry: './src/main.js',
-
   plugins: [
     new webpack.LoaderOptionsPlugin(),
     new webpack.optimize.UglifyJsPlugin({ sourceMap: true, comments: false }),
@@ -11,27 +8,6 @@ module.exports = {
       'process.env.NODE_ENV': JSON.stringify('production')
     })
   ],
-
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
-  },
-
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            plugins: 'transform-runtime',
-            presets: ['env', 'react']
-          }
-        }
-      }
-    ]
-  },
 
   devtool: 'source-map'
 }
