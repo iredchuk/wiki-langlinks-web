6897740cb02e6921b3d1c6108180733fa34c0db0
Igor Redchuk <redchuk@gmail.com>
2017-07-30 15:31:25 +0200

    Make possible to use clean dist/
    * Use html-webpack-plugin to create index.html from template
    * Use injectGlobal to create global CSS

--- src/components/app.js
+++ src/components/app.js
@@ -5,6 +5,7 @@ import Search from './search'
 import Results from './results'
 
 const Container = styled.div`
+  line-height: 120%;
   margin: 10px 20px 0 20px;
   padding: 20px;
 
--- src/main.js
+++ src/main.js
@@ -1,5 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import AppContainer from './components/app-container'
+import { injectGlobal } from 'styled-components'
+
+injectGlobal`
+  body {
+    margin: 0;
+    height: 100vh;
+    box-sizing: border-box;
+    background-color: #eee;
+  }
+
+  *, :after, :before {
+    box-sizing: inherit;
+  }
+`
 
 ReactDOM.render(<AppContainer />, document.querySelector('#app'))
